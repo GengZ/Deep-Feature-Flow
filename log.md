1f5bfdc980254d93ff08d3a2e7cbc89a15e9c4cb
GengZ <934113912@qq.com>
2017-12-15 18:37:50 +0800

    change test model to pretrained model

---
+++ log.md
@@ -0,0 +1,6 @@
+> used in msra due to limit storage on msravcg10
+##### 15/Dec/2017
+---
+**TODO:** 
+* test rfcn performance 
+* 
--- rfcn/test.py
+++ rfcn/test.py
@@ -47,8 +47,10 @@ def main():
 
     logger, final_output_path = create_logger(config.output_path, args.cfg, config.dataset.test_image_set)
 
+    # model_path = os.path.join(final_output_path, '..', '_'.join([iset for iset in config.dataset.image_set.split('+')]), config.TRAIN.model_prefix)
+    model_path = './model/rfcn_vid-0000.params'
     test_rcnn(config, config.dataset.dataset, config.dataset.test_image_set, config.dataset.root_path, config.dataset.dataset_path,
-              ctx, os.path.join(final_output_path, '..', '_'.join([iset for iset in config.dataset.image_set.split('+')]), config.TRAIN.model_prefix), config.TEST.test_epoch,
+              ctx, model_path, config.TEST.test_epoch,
               args.vis, args.ignore_cache, args.shuffle, config.TEST.HAS_RPN, config.dataset.proposal, args.thresh, logger=logger, output_path=final_output_path)
 
 if __name__ == '__main__':
